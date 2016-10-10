fc67e2f1fcff69168db4a6d1b6d09381d10fb337
Medscootsman <murraylyne2@Gmail.com>
2016-10-10 16:09:54 +0100

    Added php script

---
+++ query1.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1607084
+ * Date: 10/10/2016
+ * Time: 15:52
+ */
+
+// connect to marvel movies
+
+$db = new mysqli(
+    'eu-cdbr-azure-north-e.cloudapp.net',
+    'baa0a9a8041f1c',
+    'ea0c77dc',
+    'acsm_eeffc4063160a9f'
+);
+
+if($db->connect_errno) {
+    die('Connectfailed['.$db->connect_error.']');
+}
+
+$sql_query = "SELECT * FROM marvelmovies WHERE yearReleased = '2014'";
+
+$result = $db->query($sql_query);
+
+while($row = $result->fetch_array()) {
+
+
+    //print out fields
+    echo "<p>" . $row['title'] . "</p>";
+}
+
+$result->dose();
+
+$db->dose();
+
