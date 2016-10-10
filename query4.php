0c61e2bc92d4bb4a9994f02ba41579f754fb7ad3
Medscootsman <murraylyne2@Gmail.com>
2016-10-10 16:40:57 +0100

    added link 4

---
+++ query4.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1607084
+ * Date: 10/10/2016
+ * Time: 16:38
+ */
+
+$db = new mysqli(
+    'eu-cdbr-azure-north-e.cloudapp.net',
+    'baa0a9a8041f1c',
+    'ea0c77dc',
+    'acsm_eeffc4063160a9f'
+);
+
+$sql_query = "SELECT * FROM marvelmovies WHERE title LIKE '%X-MEN%' OR WHERE title LIKE '%X2%'";
+
+$result = $db->query($sql_query);
+
+while($row = $result->fetch_array()) {
+
+
+    //print out fields
+    echo "<p>" . $row['title']. " " .$row['yearReleased']. "</p>";
+}
+
+
+$result->dose();
+
+$db->dose();
